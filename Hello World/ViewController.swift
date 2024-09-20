ce2c8d29d9dc5026ebe14d6549e27b0ffa52af55
theArchDeluxe <156505949+theArchDeluxe@users.noreply.github.com>
2024-09-20 20:31:50 +0300

    Added Show/Hide button.

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -2,18 +2,30 @@
 //  ViewController.swift
 //  Hello World
 //
-//  Created by Alexey Elfimov on 20.09.2024.
+//  Created by Alexey Yelfimov on 20.09.2024.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greetingLabel.isHidden.toggle()
+        greetingButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func greetingButtonDidTapped(_ sender: UIButton) {
+        greetingLabel.isHidden.toggle()
+        sender.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting", for: .normal
+        )
+        
+    
+    }
+    
 }
 
